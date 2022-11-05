13e026f5d5182f7a7724234f9345a6678145fde1
Rehannriaz <u2021438@giki.edu.pk>
2022-11-05 16:02:09 +0500

    vecArr being used

--- Assignment4/CS221-A4.cpp
+++ Assignment4/CS221-A4.cpp
@@ -3,7 +3,6 @@
 class charArr
 {
 private:
-    char **arr;
     vector<vector<char>> vecArr;
     TrieClass Words;
     int rows, cols, rowCount;
@@ -12,33 +11,11 @@ public:
     charArr(TrieClass &a, int R)
     {
         rows = R;
-
         cols = 20;
         Words = a;
-        arr = new char *[rows];
-        for (int i = 0; i < rows; i++)
-        {
-            arr[i] = new char[cols];
-        }
-    }
-    // void insertion(string Word)
-    // {
-    //     static int J = 0;
-    //     int j;
-    //     for (int j = 0; j < Word.length(); j++)
-    //         arr[J][j] = Word[j];
-    //     arr[J][j + 1] = '\0';
-    //     J++;
-    //     rowCount = J;
-    //     for (int i = 0; i < rowCount; i++)
-    //     {
 
-    //         for (int a = 0; a < cols; a++)
-    //             cout << arr[i][a] << " ";
-    //         cout << "\n";
-    //     }
-    //     cout << "\n\n\n\n";
-    // }
+    }
+
     void insertion(string Word)
     {
         static int staticJ = 0;
@@ -49,13 +26,6 @@ public:
         vecArr[staticJ][j + 1] = '\0';
         staticJ++;
         rowCount = staticJ;
-        for (int i = 0; i < vecArr.size(); i++)
-        {
-
-            for (int a = 0; a < vecArr[i].size(); a++)
-                cout << vecArr[i][a];
-            cout << "\n";
-        }
     }
     void compareWord()
     {
@@ -65,14 +35,14 @@ public:
         {
             for (int j = 0; i < vecArr[i].size(); j++)
             {
-                temp += arr[i][j];
+                temp += vecArr[i][j];
                 if (Words.search(temp))
                 {
                     cout << temp << " ";
                     temp2 = temp;
                     temp = "";
                 }
-                if (Words.search(temp2))
+                else if (Words.search(temp2))
                 {
                     cout << temp2 << " ";
                     temp = temp2;
@@ -88,8 +58,8 @@ int main()
     TrieClass a;
     fileInput iFile(a);
     charArr b(a, 10);
-    b.insertion("hello");
-    b.insertion("zones");
-    b.insertion("hello4");
+    b.insertion("dogxbxxxnoon");
+    // b.insertion("zones");
+    b.insertion("rhellotherex");
     b.compareWord();
 }
